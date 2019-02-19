4a2fdd7d0f6ee34e9884eb813dc07c7afcf72851
Andrew McRae <amcrae@google.com>
2019-02-19 12:05:56 +1100

    Added license header.

--- lcd/lcd.go
+++ lcd/lcd.go
@@ -1,3 +1,17 @@
+// Copyright 2019 Google LLC
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     https://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package lcd
 
 import (
@@ -342,6 +356,7 @@ func rawSample(img image.Image, d *Digit, slist sample) int {
 	return 0x10000 - gacc/len(slist)
 }
 
+// Calibrate calculates the on and off values from the image provided.
 func (l *LcdDecoder) Calibrate(img image.Image) {
 	for _, d := range l.digits {
 		lcd := d.lcd
